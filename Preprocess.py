5a7e09ba66483e15ec9a115dac5c475f2e36fadf
ShuiZhuyuAlex <shuizhuyualex@gmail.com>
2019-07-14 00:54:55 -0400

    a

--- Preprocess.py
+++ Preprocess.py
@@ -26,12 +26,14 @@ def creatNormalizer(fold, method):
     data_path = os.path.join(vocabulary_path, data_name)
     if os.path.exists(data_path):
         phi = np.load(data_path)
+        print("Successfully Load Data for fold: {f}".format(f=str(fold)))
         if method == 'minmaxtest':
             normalizer = preprocessing.MinMaxScaler()
         elif method == 'scale':
             normalizer = preprocessing.StandardScaler()
         else:
             normalizer = preprocessing.Normalizer()
+        print("Start Fit with method: {m}".format(m=str(method)))
         normalizer.fit(phi)
         normalizer_name = 'fold_' + str(fold) + '_' + method + '.p'
         normalizer_path = os.path.join(root_path, normalizer_name)
