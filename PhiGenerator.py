d9720bf2eee757a5d0b10a44fe711d1c884079eb
ShuiZhuyuAlex <shuizhuyualex@gmail.com>
2019-07-21 01:28:11 -0400

    sss

--- PhiGenerator.py
+++ PhiGenerator.py
@@ -1,9 +1,12 @@
 import os
 import sys
+import json
 import pickle
 import numpy as np
 from Preprocess import Norm
+from Configure import data_path
 from Configure import file_path
+from Configure import label_path
 from Configure import vocabulary_path
 from sklearn.cluster import MiniBatchKMeans
 
@@ -52,15 +55,48 @@ def kMeansPhiGenerator(fold, X, method, clusters):
 
     return phi
 
+def dataFactory(index, X, method, clusters):
+    X = list()
+    Y = list()
+    for user in index[0]:
+        filename = user + '.features_labels.csv'
+        lpath = os.path.join(label_path, filename)
+        temppath= os.path.join(data_path, user)
+        df = pd.read_csv(lpath, header=0, index_col=0)
+        for ts in df.index:
+            y = np.array(list(map(float, df.loc[ts])))
+            sname = str(ts) + '.sound.mfcc'
+            dpath = os.path.join(temppath, sname)
+            if os.path.exists(dpath):
+                temp = list()
+                with open(dpath, 'r') as sf:
+                    for frame in sf.readlines():
+                        frame = frame.strip(',\n')
+                        frame = list(map(float, frame.split(',')))
+                        frame = np.array(frame)
+                        temp.append(frame)
+                raw_data = np.array(temp)
+                x = kMeansPhiGenerator(fold, raw_data, method, clusters)
+                Y.append(y)
+                X.append(x)
+            else:
+                print("Data Missing For: {file}".format(file = dpath))
+                continue
+    Y = np.array(Y)
+    X = np.array(X)
+    return X, Y
+
 if __name__ == '__main__':
-    folds = ['0', '1', '2', '3', '4']
-    clusters = [50, 60, 70, 80, 90, 100]
-    method = ['minmaxtest', 'scale', 'norm']
-    # args = sys.argv[1].split(',')
-    # fold = args[0]
-    # method = args[1]
-    # clusters = int(args[2])
-    for f in folds:
-        for c in clusters:
-            for m in method:
-                kMeansTrain(f, m, c)
+    with open('train_index.json', 'r') as index_file:
+        all = json.load(index_file)
+    index = all['0']
+    x, y = dataPrepare(index, '0', 'scale', 70)
+    print(x.shape)
+    print(y.shape)
+    # folds = ['0', '1', '2', '3', '4']
+    # clusters = [50, 60, 70, 80, 90, 100]
+    # method = ['minmaxtest', 'scale', 'norm']
+    # for f in folds:
+    #     for c in clusters:
+    #         for m in method:
+    #             kMeansTrain(f, m, c)
--- Train.py
+++ Train.py
@@ -10,38 +10,6 @@ from Configure import model_path
 from Configure import label_path
 from PhiGenerator import kMeansPhiGenerator
 
-
-def dataPrepare(index, fold, method, clusters):
-    X = list()
-    Y = list()
-    for user in index:
-        filename = user + '.features_labels.csv'
-        lpath = os.path.join(label_path, filename)
-        temppath= os.path.join(data_path, user)
-        df = pd.read_csv(lpath, header=0, index_col=0)
-        for ts in df.index:
-            y = np.array(list(map(float, df.loc[ts])))
-            sname = str(ts) + '.sound.mfcc'
-            dpath = os.path.join(temppath, sname)
-            if os.path.exists(dpath):
-                temp = list()
-                with open(dpath, 'r') as sf:
-                    for frame in sf.readlines():
-                        frame = frame.strip(',\n')
-                        frame = list(map(float, frame.split(',')))
-                        frame = np.array(frame)
-                        temp.append(frame)
-                raw_data = np.array(temp)
-                x = kMeansPhiGenerator(fold, raw_data, method, clusters)
-                Y.append(y)
-                X.append(x)
-            else:
-                print("Data Missing For: {file}".format(file = dpath))
-                continue
-    Y = np.array(Y)
-    X = np.array(X)
-    return X, Y
-
 def model(X, Y, cla):
     if cla == 'SVM':
         pass
@@ -52,9 +20,4 @@ def model(X, Y, cla):
 
 
 if __name__ == '__main__':
-    with open('train_index.json', 'r') as index_file:
-        all = json.load(index_file)
-    index = all['0']
-    x, y = dataPrepare(index, '0', 'scale', 70)
-    print(x.shape)
-    print(y.shape)
+    
